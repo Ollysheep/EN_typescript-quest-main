334ee8fb5bf168cf031a20ee06e1c8a120a85dc3
Kristina Slavkovic <137336285+Ollysheep@users.noreply.github.com>
2023-11-22 12:17:39 +0100

    Boss_Level_Finished

--- src/index.ts
+++ src/index.ts
@@ -44,7 +44,9 @@ class Hero {
   }
 
   attack(opponent: Hero): void {
+    console.log(`${this.getName()} attacks ${opponent.getName()}.`);
     opponent.takeDamage(this.heroPower);
+    console.log(`${opponent.getName()} took ${this.heroPower} damage.`);
   }
 
   takeDamage(damage: number): void {
@@ -52,6 +54,7 @@ class Hero {
     if (this.heroLife < 0) {
       this.heroLife = 0;
     }
+    console.log(`${this.getName()} took ${damage} damage.`);
   }
 
   isAlive(): boolean {
@@ -60,22 +63,26 @@ class Hero {
 }
 
 // Create instances of Hero
-const kriss = new Hero("Kriss", 25, 80);
-const ani = new Hero("Ani", 97, 230);
+const kriss = new Hero("Kriss", 31, 62);
+const ani = new Hero("Ani", 65, 130);
 
 // Check initial status
+console.log("----------------------");
+console.log("Battle One: Kriss vs. Ani");
 console.log(`Initial status:`);
 console.log(
   `${kriss.getName()} life: ${kriss.getLife()}, ${ani.getName()} life: ${ani.getLife()}`
 );
+console.log("----------------------");
 
 // Kriss attacks Ani then reverse
 kriss.attack(ani);
-
 ani.attack(kriss);
+console.log("----------------------");
 
 // Check status after the attack
-console.log(`Status after attack:`);
+console.log("Status after Battle One:");
+console.log("----------------------");
 console.log(
   `${kriss.getName()} life: ${kriss.getLife()}, ${ani.getName()} life: ${ani.getLife()}`
 );
@@ -83,6 +90,7 @@ console.log(
 // Check if heroes are alive
 console.log(`${kriss.getName()} is alive: ${kriss.isAlive()}`);
 console.log(`${ani.getName()} is alive: ${ani.isAlive()}`);
+console.log("----------------------");
 
 // HeroAxe, HeroSword, HeroSpear inherit from Hero class and implement specialized attacks based on opponent types
 class HeroAxe extends Hero {
@@ -128,32 +136,39 @@ class HeroSpear extends Hero {
 }
 
 // Create instances of the three classes
-const axeHero = new HeroAxe("Pistache", 58, 230);
-const swordHero = new HeroSword("Maki", 142, 567);
-const spearHero = new HeroSpear("Luci", 28, 110);
+const axeHero = new HeroAxe("Pistache", 367, 1425);
+const swordHero = new HeroSword("Maki", 1536, 3872);
+const spearHero = new HeroSpear("Luci", 449, 1212);
 
-/* Test the attack methods
- *axeHero.attack(swordHero);
- *swordHero.attack(spearHero);
- *spearHero.attack(axeHero);
- *
- * Check status after the attacks
- *console.log(`Status after attacks:`);
- *console.log(
- *  `${axeHero.getName()} life: ${axeHero.getLife()}, ${swordHero.getName()} life: ${swordHero.getLife()}, ${spearHero.getName()} life: ${spearHero.getLife()}`
- *);
- *
- * Check if heroes are alive
- *console.log(`${axeHero.getName()} is alive: ${axeHero.isAlive()}`);
- *console.log(`${swordHero.getName()} is alive: ${swordHero.isAlive()}`);
- *console.log(`${spearHero.getName()} is alive: ${spearHero.isAlive()}`); */
+// Test the attack methods
+console.log("Battle Two: Pistache vs. Maki vs. Luci");
+console.log(`Initial status:`);
+console.log(
+  `${axeHero.getName()} life: ${axeHero.getLife()}, ${swordHero.getName()} life: ${swordHero.getLife()}, ${spearHero.getName()} life: ${spearHero.getLife()}`
+);
+console.log("----------------------");
+axeHero.attack(swordHero);
+swordHero.attack(spearHero);
+spearHero.attack(axeHero);
+
+// Check status after Battle Two
+console.log("----------------------");
+console.log("Status after Battle Two Round One:");
+console.log(
+  `${axeHero.getName()} life: ${axeHero.getLife()}, ${swordHero.getName()} life: ${swordHero.getLife()}, ${spearHero.getName()} life: ${spearHero.getLife()}`
+);
+
+// Check if heroes are alive
+console.log(`${axeHero.getName()} is alive: ${axeHero.isAlive()}`);
+console.log(`${swordHero.getName()} is alive: ${swordHero.isAlive()}`);
+console.log(`${spearHero.getName()} is alive: ${spearHero.isAlive()}`);
+console.log("----------------------");
 
 // The loop for simultaneous fights and determining the winner
 const heroes = [axeHero, swordHero, spearHero]; // Array containing instances of Hero subclasses
 
-let allDead = false;
-while (!allDead) {
-  // Simultaneous attacks
+let aliveCount = heroes.length; // Track the count of alive heroes
+while (aliveCount > 1) {
   for (let i = 0; i < heroes.length; i++) {
     for (let j = i + 1; j < heroes.length; j++) {
       if (heroes[i].isAlive() && heroes[j].isAlive()) {
@@ -163,16 +178,23 @@ while (!allDead) {
     }
   }
 
-  // Check if all heroes are dead
-  allDead = heroes.every((hero) => !hero.isAlive());
+  aliveCount = heroes.filter((hero) => hero.isAlive()).length;
 }
+
+console.log("----------------------");
+console.log("Status after Battle Two Final One:");
+console.log(
+  `${axeHero.getName()} life: ${axeHero.getLife()}, ${swordHero.getName()} life: ${swordHero.getLife()}, ${spearHero.getName()} life: ${spearHero.getLife()}`
+);
 
 // Determine the result
 const aliveHeroes = heroes.filter((hero) => hero.isAlive());
 
-if (aliveHeroes.length === 0) {
-  console.log("It's a draw.");
-} else {
+if (aliveHeroes.length === 1) {
   const winner = aliveHeroes[0];
-  console.log(`${winner.getName()} wins.`);
+  console.log("----------------------");
+  console.log(`${winner.getName()} is the winner.`);
+} else {
+  console.log("----------------------");
+  console.log("It's a draw.");
 }
